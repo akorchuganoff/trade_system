81249ce5a3db87ee09d6dd4970a5371c3f44312f
DavydenkoGr <grigorydavydenko@yandex.ru>
2021-06-09 11:17:29 +0700

    new db

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from data import db_session
 from data.offer import Offer
 from data.user import User
+from forms.registration import RegisterForm, LoginForm
 from flask import Flask, render_template, redirect, request, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_restful import Api, Resource
-from personal_account import Account
 
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SECRET_KEY'] = 'DK_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -26,7 +27,6 @@ def logout():
     return redirect("/")
 
 
-
 @app.route("/")
 def system():
     return render_template("system.html")
@@ -37,9 +37,16 @@ def account():
     return render_template("account.html")
 
 
+@app.route("/registration")
+def registration():
+    form = RegisterForm()
+    return render_template("registration.html", form=form)
+
+
 def main():
     db_session.global_init("db/trade_system.db")
     app.run(host='127.0.0.1', port=8000, debug=True)
+
 
 if __name__ == '__main__':
     main()
--- personal_account.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from flask_restful import Resource
-from flask_login import current_user
-
-
-class Account(Resource):
-    def get(self):
-        return jsonify({'current_user': current_user.id})
