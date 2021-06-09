52902cecaef2073d1f43e2f173cc948c006d0e4e
DavydenkoGr <grigorydavydenko@yandex.ru>
2021-06-09 14:38:51 +0700

    search form

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from data import db_session
 from data.offer import Offer
 from data.user import User
 from forms.add_offer_form import AddOfferForm
+from forms.search import SearchForm
 from forms.registration import RegisterForm, LoginForm
 from flask import Flask, render_template, redirect, request, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
@@ -31,14 +32,12 @@ def logout():
 @app.route("/system")
 @app.route("/", methods=['GET', 'POST'])
 def system():
-    if request.method == 'GET':
-        db_sess = db_session.create_session()
-        offers = db_sess.query(Offer).all()[:5]
-        print(offers[0].name)
-        return render_template("system.html", n=max(0, len(offers)), offers=offers)
-    elif request.method == 'POST':
+    form = SearchForm()
+    if request.method == "GET":
         pass
-
+    db_sess = db_session.create_session()
+    offers = db_sess.query(Offer).all()[:5]
+    return render_template("system.html", n=max(0, len(offers)), offers=offers, form=form)
 
 
 @app.route("/account")
@@ -85,15 +84,11 @@ def login():
                                form=form)
     return render_template("login.html", form=form)
 
-@app.route('/add_offer')
-def add_offer():
-    form=AddOfferForm()
-    return render_template("add_offer.html", form=form)
 
 def main():
-    # db_sess = db_session.create_session()
-    # db_sess.commit()
     db_session.global_init("db/trade_system.db")
+    db_sess = db_session.create_session()
+    db_sess.commit()
     app.run(host='127.0.0.1', port=8000, debug=True)
 
 
