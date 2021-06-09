7f3a144e0d687facfb161c494138012e9a79bd40
akorchuganoff <akorchuganoff@yandex.ru>
2021-06-09 11:03:52 +0700

    1.0

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from data.user import User
 from flask import Flask, render_template, redirect, request, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_restful import Api, Resource
+from personal_account import Account
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -24,6 +26,7 @@ def logout():
     return redirect("/")
 
 
+
 @app.route("/")
 def system():
     return render_template("system.html")
@@ -37,7 +40,6 @@ def account():
 def main():
     db_session.global_init("db/trade_system.db")
     app.run(host='127.0.0.1', port=8000, debug=True)
-
 
 if __name__ == '__main__':
     main()
--- personal_account.py
+++ personal_account.py
@@ -2,6 +2,7 @@ from flask import jsonify
 from flask_restful import Resource
 from flask_login import current_user
 
-class Account():
+
+class Account(Resource):
     def get(self):
-        return
+        return jsonify({'current_user': current_user.id})
