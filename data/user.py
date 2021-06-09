727048696e4f6808724c60389470833afefb048a
DavydenkoGr <grigorydavydenko@yandex.ru>
2021-06-09 10:51:52 +0700

    nation + registration html

--- data/user.py
+++ data/user.py
@@ -14,6 +14,7 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=False)
+    nation = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     money = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
     offer = orm.relation("Offer", back_populates='user')
 
